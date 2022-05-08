a2d880d4eae3e5e82470d4b622d910110fe007fa
Cleverson Henriques <henriquescleverson@gmail.com>
2022-05-08 18:04:04 -0300

    Mapeando entidades

--- src/main/java/com/dio/santander/bankline/api/model/Movimentaçao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentaçao.java
@@ -1,5 +1,45 @@
 package com.dio.santander.bankline.api.model;
 
 public class Movimentaçao {
+	private Integer id;
+	private localDataTime dataHora;
+	private String descricao;
+	private Double valor;
+	private MovimentaçaoTipo tipo;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public localDataTime getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(localDataTime dataHora) {
+		this.dataHora = dataHora;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	public MovimentaçaoTipo getTipo() {
+		return tipo;
+	}
+	public void setTipo(MovimentaçaoTipo tipo) {
+		this.tipo = tipo;
+	}
 
 }
