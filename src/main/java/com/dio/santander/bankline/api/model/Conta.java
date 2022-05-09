313802ebb9858eba9d696d0a04db8e6059cec286
Cleverson Henriques <henriquescleverson@gmail.com>
2022-05-08 21:26:46 -0300

    .

--- src/main/java/com/dio/santander/bankline/api/model/Conta.java
+++ src/main/java/com/dio/santander/bankline/api/model/Conta.java
@@ -1,9 +1,15 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
+	@Column(name = "conta_numero")
 	private Long numero;
-	private Double saldo;
 	
+	@Column(name = "conta_saldo")
+	private Double saldo;
 	public Long getNumero() {
 		return numero;
 	}
@@ -16,5 +22,6 @@ public class Conta {
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
 	}
-
+	
 }
+	
